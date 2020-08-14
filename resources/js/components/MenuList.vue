23cfcbaf75e9767d4625435b24fc903a822eaf36
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-08-14 14:55:53 +0300

    :Scoped Slots - initial commit.

---
+++ resources/js/components/MenuList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <ul>
+<!--			<li v-for="item in items" v-text="item">-->
+			<li v-for="item in items">
+<!--                <slot :item="item">{{ item }}</slot>-->
+                <slot name="menu-item" :item="item">{{ item }}</slot>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['items'],
+		// mounted() {
+        //     console.log('Component mounted.')
+        // }
+    }
+</script>
